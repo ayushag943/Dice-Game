dfd0f9fda40893b5a0d1e885adb70cc2db7b63e4
Ayush Goyal <75775285+ayushag943@users.noreply.github.com>
2022-05-24 17:28:50 +0530

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 // var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png";; //images/dice1.png - images/dice6.png
+var randomImageSource1 = "dice" + randomNumber1 + ".png";; //dice1.png - dice6.png
 
 // var image1 = document.querySelectorAll("img")[0];
 
@@ -12,7 +12,7 @@ document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
